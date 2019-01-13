0fc36fd852d435b7648e2a631198249d865b7d66
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2019-01-12 23:29:25 -0200

    Adding mongolab

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/knowledge_stats', {useNewUrlParser: true})
+if (!process.env.PORT) {
+    const { mongoURI } = require('../.env')
+} else {
+    mongoURI = process.env.MONGO_URI
+}
+mongoose.connect(mongoURI, {useNewUrlParser: true})
     .catch(e => {
         const msg = 'Não foi possível conectar com o MongoDB'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
